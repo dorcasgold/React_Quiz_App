8ce0a0ab0e5d642082f627669a68559a593f854d
odetayo dorcas <odetayodorcas123@gmail.com>
2024-05-21 05:46:25 +0100

    create jsx and style it

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -3,7 +3,19 @@ import React from "react"
 import './Quiz.css'
 function Quiz() {
   return (
-    <>Quiz</>
+    <div className="container">
+      <h1>Quiz App</h1>
+      <hr />
+      <h2>In what year was the first iPhone released?</h2>
+      <ul>
+        <li>2003</li>
+        <li>2005</li>
+        <li>2000</li>
+        <li>2007</li>
+      </ul>
+      <button>Next</button>
+      <div className="index">1 of 5 questions</div>
+    </div>
   )
 }
 
