ed7c69c4dce98693603a23227024073d7907bba0
odetayo dorcas <odetayodorcas123@gmail.com>
2024-05-21 05:59:50 +0100

    create and export data file of questions and answers

---
+++ src/assets/data.js
@@ -0,0 +1,43 @@
+export const data = [
+  {
+    questions: 'What is the capital of France?',
+    option1: 'Madrid',
+    option2: 'Paris ',
+    option3: 'Rome',
+    option4: 'Berlin',
+    ans: 2
+  },
+  {
+    questions: 'Which planet is known as the Red Planet?',
+    option1: 'Mars ',
+    option2: 'Venus ',
+    option3: 'Saturn',
+    option4: 'Jupiter',
+    ans: 1
+  },
+  {
+    questions: 'In what year was the first iPhone released?',
+    option1: '1999',
+    option2: '2007 ',
+    option3: '2005',
+    option4: '2004',
+    ans: 2
+  },
+  {
+    questions: 'What is the largest mammal in the world?',
+    option1: 'Blue Whale',
+    option2: 'Elephant ',
+    option3: 'Great White Shark',
+    option4: 'Elephant',
+    ans: 1
+  },
+  {
+    questions: 'What is the primary color of bananas?',
+    option1: 'Red',
+    option2: 'Blue ',
+    option3: 'Yellow',
+    option4: 'Purple',
+    ans: 3
+  },
+
+]
